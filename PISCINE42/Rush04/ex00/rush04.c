77b5dd38d677c4f350055aa554e6e6f8d38ea610
Mochi Mochi <77756115+SweetMochi06@users.noreply.github.com>
2022-07-17 18:11:54 +0200

    Add files via upload
    Petit rush fait avec mon mate

---
+++ PISCINE42/Rush04/ex00/rush04.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danperei <danperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 10:02:21 by danperei          #+#    #+#             */
+/*   Updated: 2022/07/17 16:40:58 by danperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	j;
+	int	i;
+
+	j = 1;
+	while (j <= y)
+	{
+		i = 1;
+		while (i <= x)
+		{
+			if ((j == 1 && i == 1) || (j == y && i == x))
+				ft_putchar('A');
+			else if ((j == 1 && i == x) || (j == y && i == 1))
+				ft_putchar('C');
+			else if ((j == 1 || j == y || i == 1 || i == x))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+}
