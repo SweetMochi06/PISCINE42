bd3132807d4c83a35d0daa658e09592d58e93c0c
Mochi Mochi <77756115+SweetMochi06@users.noreply.github.com>
2022-07-21 17:37:44 +0200

    they are foctionnal

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danperei <danperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 12:37:57 by danperei          #+#    #+#             */
+/*   Updated: 2022/07/21 17:27:25 by danperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+//#include <string.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
+
+/*int main()
+{
+    char    src[100] = "Mochi";
+    char    dest[100] = "This is ";
+
+    ft_strcat(dest, src);
+    printf("%s\n", (dest));
+}*/
