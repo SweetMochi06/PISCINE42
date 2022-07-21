f38873f0346817513b62d500ce40940d104bc053
Mochi Mochi <77756115+SweetMochi06@users.noreply.github.com>
2022-07-21 17:38:31 +0200

    They are foctional (ex04, ex05 not done)
    They are foctional

---
+++ PISCINE42/C03/ex00/ft_strcmp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danperei <danperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 12:30:02 by danperei          #+#    #+#             */
+/*   Updated: 2022/07/21 17:24:25 by danperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+//#include <string.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+/*int	main()
+{
+	char	s1[] = "aGdsaga";
+	char	s2[] = "aGdsaga";
+
+	printf("%d\n", ft_strcmp(s1, s2));
+	printf("%d\n", strcmp(s1, s2));
+}*/
