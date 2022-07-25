30df59cf339f70af9eefbc271955a342d7f73ec0
Mochi Mochi <77756115+SweetMochi06@users.noreply.github.com>
2022-07-25 16:21:19 +0200

    Fixed
    Fixed

--- PISCINE42/C03/ex01/ft_strncmp.c
+++ PISCINE42/C03/ex01/ft_strncmp.c
@@ -6,32 +6,31 @@
 /*   By: danperei <danperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 17:12:01 by danperei          #+#    #+#             */
-/*   Updated: 2022/07/21 17:25:42 by danperei         ###   ########.fr       */
+/*   Updated: 2022/07/25 16:02:23 by danperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-//#include <string.h>
+#include <stdio.h>
+#include <string.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while ((i < n) && s1[i] && s2[i] != '\0')
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
-	}
+	if (n == 0)
+		return (0);
+	while ((i < n - 1) && (s1[i] && s2[i] != '\0') && (s1[i] == s2[i]))
+			i++;
 	return (s1[i] - s2[i]);
 }
 
-/*int main()
+int main (void)
 {
-    char	s1[] = "aGdsaga";
-	char	s2[] = "aGdsaga";
+    char	s1[] = "";
+	char	s2[] = "n";
 
     printf("%d\n", ft_strncmp(s1, s2, 10));
     printf("%d\n", strncmp(s1, s2, 10));
-}*/
+}
